8eccebddc759fa066c185f54aded66ac427c5bbf
Sarah Dylan <SarahmDylan@gmail.com>
2022-07-07 18:15:19 -0700

    Add method to select random word
    Method picks a random number between 0 and length of word_array
    and then returns a word at that index.

--- lib/game.rb
+++ lib/game.rb
@@ -1,14 +1,20 @@
-words = File.open('google-10000-english-no-swears.txt')
-
 class Game
   require 'json'
+  attr_accessor :word_array
 
-  def initialize() end
+  def initialize
+    @word_array = build_word_array
+  end
 
-  def build_word_array(file)
+  def build_word_array
+    file = File.open('google-10000-english-no-swears.txt')
     file.readlines.map(&:chomp).select do |word|
       word.length >= 5 && word.length <= 12
     end
+  end
+
+  def select_random_word
+    @word_array[rand(@word_array.length)]
   end
 
   def to_json(*args)
@@ -17,7 +23,5 @@ class Game
     }.to_json(*args)
   end
 
-  def self.json_create(object)
-    new(*object[])
-  end
+  def self.json_create(object) end
 end
